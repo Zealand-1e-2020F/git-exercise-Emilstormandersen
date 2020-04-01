8abe82038bbf1287f296d91769c1176298bbcd05
Emilstormandersen <Emil.storm.andersen@gmail.com>
2020-04-01 10:40:11 +0200

    Added Test Writeline
    Yes baby

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
 
             myClass myclass = new myClass();
             
